0cff6d547df4ba366555f28914ad02dcb874c6e9
Bielfla27 <gabibielmft@outlook.com>
2022-02-16 08:49:41 -0300

    adicionando mais funções

--- Função.sql
+++ Função.sql
@@ -16,3 +16,37 @@ END;
 
 --retornando valor da função 
 select ROUND(FN_mediaPond(5,5,10,1)) from sys.dual;
+
+
+--CRIANDO FUNÇÃO QUE RETORNA NOME
+CREATE OR REPLACE FUNCTION fnNome(p_id in number)
+    return varchar IS
+    v_nome varchar(20);
+Begin
+    SELECT a.FIRST_NAME
+    INTO v_nome
+    FROM hr.EMPLOYEES a 
+    WHERE a.EMPLOYEE_ID = p_id;
+    
+    return v_nome;
+end;
+
+--chamadno a função nome
+select fnNome(101) From dual;
+
+
+CREATE OR REPLACE FUNCTION fnAumento(p_pct in number, p_id_func in number)
+    RETURN NUMBER IS
+    v_Sal_novo number(20);
+BEGIN
+    select ((a.salary/100)*p_pct) + a.salary
+    into v_Sal_novo
+    From hr.employees a
+    where a.employee_id = p_id_func;
+    
+    return v_sal_novo;
+end;
+
+--testando a função
+SELECT a.first_name , a.salary sal_antigo, fnAumento(10,a.employee_id) sal_novo
+FROM hr.employees a;
